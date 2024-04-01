07619fe40720f7e4dffeeda103738b83a2e4321c
Artem Ganev <artem_ganev@epam.com>
2024-04-01 19:49:39 -0400

    Update Terraform config

--- main.tf
+++ main.tf
@@ -2,30 +2,30 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.92.0"
+      version = "~> 3.97.1"
     }
   }
 
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.7.0"
 }
 
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "front_end_rg" {
-  name     = "rg-frontend-sand-ne-001"
-  location = "westus2"
+resource "azurerm_resource_group" "learn_azure_rg" {
+  name     = "rg-learn-azure-eu-0"
+  location = "eastus"
 }
 
-resource "azurerm_storage_account" "front_end_storage_account" {
-  name                     = "artemganevstorageacc0"
-  location                 = azurerm_resource_group.front_end_rg.location
+resource "azurerm_storage_account" "learn_azure_storage_account" {
+  name                     = "learnstorageaccount0"
+  location                 = azurerm_resource_group.learn_azure_rg.location
 
   account_replication_type = "LRS"
   account_tier             = "Standard"
   account_kind             = "StorageV2"
-  resource_group_name      = azurerm_resource_group.front_end_rg.name
+  resource_group_name      = azurerm_resource_group.learn_azure_rg.name
 
   static_website {
     index_document = "index.html"
