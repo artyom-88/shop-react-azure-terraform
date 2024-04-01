2c68659e3874dbc2ada4bba82a39e2da34b5d28d
Artem Ganev <artem_ganev@epam.com>
2024-04-01 11:43:48 -0400

    Update Terraform config

--- main.tf
+++ main.tf
@@ -13,21 +13,42 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "front_end_rg" {
-  name     = "rg-frontend-sand-ne-001"
+resource "azurerm_resource_group" "learn_azure_rg" {
+  name     = "learn-azure-rg"
   location = "westus2"
 }
 
-resource "azurerm_storage_account" "front_end_storage_account" {
-  name                     = "artemganevstorageacc0"
-  location                 = azurerm_resource_group.front_end_rg.location
+resource "azurerm_storage_account" "learn_azure_storage_account" {
+  name                     = "learnazurestorageaccount00"
+  location                 = azurerm_resource_group.learn_azure_rg.location
 
   account_replication_type = "LRS"
   account_tier             = "Standard"
   account_kind             = "StorageV2"
-  resource_group_name      = azurerm_resource_group.front_end_rg.name
+  resource_group_name      = azurerm_resource_group.learn_azure_rg.name
 
   static_website {
     index_document = "index.html"
   }
+}
+
+resource "azurerm_app_service_plan" "learn_azure_service_plan" {
+  name                = "learn-azure-functions-service-plan"
+  location            = azurerm_resource_group.learn_azure_rg.location
+  resource_group_name = azurerm_resource_group.learn_azure_rg.name
+  kind                = "Linux"
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_function_app" "learn_azure_functions_app" {
+  name                       = "fa-products-service-sand-ne-001"
+  location                   = azurerm_resource_group.learn_azure_rg.location
+  resource_group_name        = azurerm_resource_group.learn_azure_rg.name
+  app_service_plan_id        = azurerm_app_service_plan.learn_azure_service_plan.id
+  storage_account_name       = azurerm_storage_account.learn_azure_storage_account.name
+  storage_account_access_key = azurerm_storage_account.learn_azure_storage_account.primary_access_key
 }
