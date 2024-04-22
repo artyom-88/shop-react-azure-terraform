8a5d15221ad56fd603f6503a44a3f2a139146f92
Artem Ganev <artem_ganev@epam.com>
2024-04-22 12:20:27 -0400

    Use CosmosDB in http-get-product-list

--- packages/functions/http-get-product-list/index.ts
+++ packages/functions/http-get-product-list/index.ts
@@ -1,19 +1,27 @@
 import { AzureFunction, Context } from '@azure/functions';
-import { AppConfigurationClient } from '@azure/app-configuration';
+import { createDbClient, DB_ID, PRODUCTS_CONTAINER_ID, QUERY_ALL, STOCK_CONTAINER_ID } from '../common';
+import { FeedResponse } from '@azure/cosmos';
+import { Product, ProductDTO, Stock } from '../types';
 
 const httpTrigger: AzureFunction = async function (context: Context): Promise<void> {
   context.log('HTTP trigger function processed a request.');
-  const connectionString = process.env.AZURE_APP_CONFIG_CONNECTION_STRING;
-  const client = new AppConfigurationClient(connectionString);
-  const { value } = await client.getConfigurationSetting({ key: 'PRODUCTS_MOCK' });
-  try {
-    context.res = {
-      status: 200,
-      body: JSON.parse(value),
-    };
-  } catch (e) {
-    context.log(e);
-  }
+  const dbClient = await createDbClient(context);
+  const productContainer = dbClient.database(DB_ID).container(PRODUCTS_CONTAINER_ID);
+  const stockContainer = dbClient.database(DB_ID).container(STOCK_CONTAINER_ID);
+  const productQueryResponse: FeedResponse<Product> = await productContainer.items.query(QUERY_ALL).fetchAll();
+  const stockQueryResponse: FeedResponse<Stock> = await stockContainer.items.query(QUERY_ALL).fetchAll();
+  const products = productQueryResponse.resources;
+  const stocks = stockQueryResponse.resources;
+
+  products.forEach((product: ProductDTO) => {
+    const productStock = stocks.find((stock) => stock.product_id === product.id);
+    product.count = productStock ? productStock.count : 0;
+  });
+
+  context.res = {
+    body: products,
+    status: 200,
+  };
 };
 
 export default httpTrigger;
